8599639fbce91d9242715edf19eaa46fdd962ed3
José Luis González Ruiz <jose.gonzalez.sge@mango.com>
2021-12-02 17:32:17 +0100

    args fix

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -2,7 +2,7 @@ const Query = {
   books: (parent, args, { db }, info) => db.books,
   authors: (parent, args, { db }, info) => db.authors,
   author: (parent, args, { db }, info) =>
-    db.authors.find((a) => a.id === parent.id),
+    db.authors.find((a) => a.id === args.id),
 };
 
 export default Query;
